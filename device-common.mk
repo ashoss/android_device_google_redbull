701ced7b22023eb23d991310dbf391db836697d4
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2024-04-18 14:46:30 +0300

    redbull: switch RenderEngine backend to SkiaVk

--- device-common.mk
+++ device-common.mk
@@ -722,6 +722,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1
 
+# Configure renderengine backend
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.renderengine.backend=skiavkthreaded
+
 BOARD_USES_QCNE := true
 
 PRODUCT_COPY_FILES += \
